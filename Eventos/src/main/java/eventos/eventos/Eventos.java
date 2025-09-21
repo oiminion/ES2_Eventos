88e3255c988c29c33ff11944b68e153da3c8e6cc
Arthur Koichi Nakao <arthurkoichi@gmail.com>
2025-09-21 12:35:21 -0300

    comeco

---
+++ Eventos/src/main/java/eventos/eventos/Eventos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package eventos.eventos;
+
+/**
+ *
+ * @author Arthur
+ */
+public class Eventos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
