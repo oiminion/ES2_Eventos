f569f857af557cfd3a6202de320d03153cda14eb
Arthur Koichi Nakao <arthurkoichi@gmail.com>
2025-09-21 12:45:25 -0300

    Conceitos

---
+++ Eventos/src/main/java/Classes/Categoria.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author Arthur
+ */
+public enum Categoria {
+    CUSTO_FIXO,
+    CUSTO_VARIAVEL;
+}
